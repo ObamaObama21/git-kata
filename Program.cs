4a11975d7b5686e605f5aca75e6d53562b7ea1cf
ObamaObama21 <theghostplayer8@gmail.com>
2022-11-17 15:33:32 +0200

    Create Program.cs
    Added main program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿static int ReturnNumbers(int a, int b)
+{
+    int multiply = a * b;
+
+    return multiply;
+}
+
+static void Main()
+{
+    int a = 4;
+    int b = 5;
+    int result = ReturnNumbers(a, b);
+    Console.WriteLine(result);
+}
+
+Main();
